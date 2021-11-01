a7c9dbdd321c50cd8a741288ce06638d119b07db
Damian Sire <damiansire@hotmail.com>
2021-10-31 22:46:23 -0300

    Add services worker

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-let time = 0;
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
 });
 
-function setColor() {
-  changeColor.style.backgroundColor = time % 2 == 1 ? "red" : "blue";
-  time++;
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
 }
-
-setInterval(setColor, 1000);
